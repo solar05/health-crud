e8bea4916944bd6faaf0d82d3a867ff4412268a6
solar05 <artem00298@gmail.com>
2020-08-03 20:38:41 +0300

    Remove heroku

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject health-crud "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
-  :uberjar-name "health-crud.jar"
+  :description "Simple crud"
+  :url "https://github.com/solar05/health-crud"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
@@ -12,6 +10,9 @@
                  [org.clojure/java.jdbc "0.7.11"]
                  [ring/ring-defaults "0.3.2"]
                  [org.postgresql/postgresql "42.2.14"]]
+  :main ^:skip-aot health-crud.core
+  :uberjar-name "health-crud.jar"
+  :target-path "target/%s"
   :plugins [[jonase/eastwood "0.3.10"]
             [lein-cljfmt "0.6.8"]
             [lein-kibit "0.1.8"]
